822eedb24c7c2e41cf5e606dd577d9bbab835951
Yan Liu <yliu@exou.com>
2015-03-05 17:57:32 -0500

    jquery test

---
+++ web/js/lol.js
@@ -0,0 +1,19 @@
+/**
+ * Created by admin on 15-03-05.
+ */
+$(document).ready(function(){
+
+    $('#btn_hello' ).click(function(){
+        var url = 'http://nevergg.info:22000';
+        $.ajax({
+            type: 'GET',
+            url: url ,
+            success: function(result) {
+                $('#p_s_hello' ).html(JSON.stringify(result));
+            },
+            error: function(jqXHR, status, error){
+                $('#p_s_hello' ).append('hello world failed, ret = ' + status + ', status = ' + jqXHR.status + '<br>');
+            }
+        });
+    });
+});
