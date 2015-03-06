776ff2c447f5f9776c525a8f8a6e8da783dee4bd
Yan Liu <yliu@exou.com>
2015-03-05 21:03:08 -0500

    first test of lol api

--- app.js
+++ app.js
@@ -5,11 +5,23 @@
 var express = require('express');
 var app = express();
 var logger = require('./logger');
+var LolApi = require('leagueapi');
+
+LolApi.init('ad849455-ed5d-48e5-a777-180e25cbdc90', 'na');
+
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+    var freeChamps = [];
 
+    LolApi.getChampions(true, function(err, champs) {
+        champs.forEach(function(champ) {
+            LolApi.Static.getChampionById(champ.id,'na',function(err, result) {
+                if(champ.freeToPlay) freeChamps.push(result.name);
+                if (freeChamps.length == 10) {res.send(freeChamps);}
+            });
+        });
+    });
+})
 
 
 var server = app.listen(28000);
